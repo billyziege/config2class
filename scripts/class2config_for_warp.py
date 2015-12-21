89ee5f1e948af99a817fd5d72b39582bf9d8e307
billyziege <brandon.s.zerbe@gmail.com>
2015-12-21 10:38:03 -0500

    Separated the warp code into its own utility.

--- scripts/class2config_for_warp.py
+++ scripts/class2config_for_warp.py
@@ -2,8 +2,9 @@ import argparse
 import sys
 from ConfigParser import SafeConfigParser
 from class_and_config_conversion import export_class_as_config
-parser = argparse.ArgumentParser(description='Finds the given class(es) and writes a config file corresponding to the init parameters for the importer.')
-parser.add_argument('class_name', nargs='+', type=str, help='The module_name, class name, and optional instance name separatd by ":".  Can be more than one if they are separated by spaces. Eg. ZCylinder:First ZCylinder.')
+from warpoptions import *
+parser.description='Finds the given class(es) and writes a config file corresponding to the init parameters for the importer.'
+parser.add_argument('class_name', nargs='+', type=str, help='The class name, and optional instance name separatd by ":".  Warp is not necessary in the args.  Can be more than one if they are separated by spaces. Eg. ZCylinder:First ZCylinder.')
 parser.add_argument('-o', dest="output_file", type=str, help='The output file path to which the output should be written.  Default is stdout.', default=None)
 args = parser.parse_args()
 
@@ -13,18 +14,18 @@ def my_parse_args(arg_string):
   and optional instance_name components.
   """
   pieces = arg_string.split(":")
-  module_name = pieces[0]
-  class_name = pieces[1]
+  class_name = pieces[0]
   instance_name = None
-  if len(pieces) > 2:
-    instance_name = pieces[2]
-  return (module_name, class_name, instance_name)
+  if len(pieces) > 1:
+    instance_name = pieces[1]
+  return (class_name, instance_name)
 
 options = {}
 
 config = SafeConfigParser()
 for arg_string in args.class_name:
-  module_name, class_name, instance_name = my_parse_args(arg_string) 
+  module_name = "warp"
+  class_name, instance_name = my_parse_args(arg_string) 
   config = export_class_as_config(module_name,class_name,instance_name,config,**options)
 
 if args.output_file is None:
